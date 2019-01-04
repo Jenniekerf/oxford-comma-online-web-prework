33682f6d6a7a40178de817214ae9d533a7d76992
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-04 18:38:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
-def oxford_comma(array)
-  if array.size == 1 
-    return array[0]
-    elsif array.size == 2
-return array.join(" and ")
-else
-  return array[0..-2].join(", ") + ", and " + array[-1]
+def oxford_comma(string)
+  if string.size == 1 
+    string.join
+    elsif string.size == 2
+    string.join(" and ")
+  else
+    string[0..-2].join(", ") + ", and " + string[-1]
 end
 end
+
 
